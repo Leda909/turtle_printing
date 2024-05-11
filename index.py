73023a54a178030eb4d030d3cc93a8e976d2d1b2
Leda909 <ko9zad@gmail.com>
2024-05-12 00:43:36 +0100

    grid

--- index.py
+++ index.py
@@ -2,25 +2,19 @@ import turtle
 tina = turtle.Turtle()
 tina.shape('turtle')
 
-your_name = input("What's your name?")
-
-tina.penup()
-tina.goto(0,150)
-tina.write("Hello, " + your_name + "!")
-tina.goto(0,100)
-tina.write("I do not draw when my pen is up!")
-tina.goto(0,50)
-
-tina.pendown()
-tina.color("blue")
-tina.write("I do draw when my pen is down!")
-tina.goto(-100,50)
-
-tina.color("purple")
-tina.goto(-100,-50)
-
-tina.color("green")
-tina.goto(0,-50)
-
+tina.write("I start at 0, 0")
 tina.color("orange")
-tina.goto(0,50)
+tina.goto(100,100)
+tina.write("This is 100, 100")
+tina.color("red")
+tina.goto(-100,100)
+tina.write("This is -100, 100")
+tina.color("green")
+tina.goto(100,-100)
+tina.write("This is 100, -100")
+tina.color("blue")
+tina.goto(-100,-100)
+tina.write("This is -100, -100")
+tina.color("violet")
+tina.goto(0,0)
+
