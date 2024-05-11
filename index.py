dfb86e05900b476b6ef185e909db83ddd2123e65
Leda909 <ko9zad@gmail.com>
2024-05-12 00:03:10 +0100

    color

--- index.py
+++ index.py
@@ -2,8 +2,19 @@ import turtle
 tina = turtle.Turtle()
 tina.shape('turtle')
 
-tina.forward(50)
+your_name = input("What's your name?")
+
+tina.penup()
 tina.left(90)
-tina.forward(50)
-tina.left(90)
-tina.forward(50)
+tina.forward(30)
+tina.color("blue")
+tina.write("Hello, " + your_name + "!")
+tina.write("What color am I now?")
+
+tina.forward(30)
+tina.color("purple")
+tina.write("What color am I now?")
+
+tina.forward(30)
+tina.color("green")
+tina.write("What color am I now?")
