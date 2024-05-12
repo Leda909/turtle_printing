f2a14195ae89587a6964707a7c1179844a153a90
Leda909 <ko9zad@gmail.com>
2024-05-12 01:10:43 +0100

    draw from array

--- index.py
+++ index.py
@@ -2,60 +2,11 @@ import turtle
 tina = turtle.Turtle()
 tina.shape('turtle')
 
-#Drawn face
-tina.penup()
-tina.begin_fill()
-tina.color('pink')
-tina.goto(30,-150)
-tina.pendown()
-tina.circle(130)
-tina.penup()
-tina.end_fill()
+number_list = [1,2,3,4,5,6,7,8,9,10] 
 
-# Draw right eye
-tina.color('white')
-tina.goto(0,0)
-tina.begin_fill()
-tina.pendown()
-tina.circle(20)
-tina.penup()
-tina.end_fill()
-tina.begin_fill()
-
-# Draw left pupil
-tina.color('black')
-tina.pendown()
-tina.circle(10)
-tina.penup()
-tina.end_fill()
-tina.forward(60)
-tina.right(45)
-tina.begin_fill()
-
-# Draw right eye
-tina.color('white')
-tina.pendown()
-tina.circle(30)
-tina.penup()
-tina.end_fill()
-
-# Draw right pupil
-tina.begin_fill()
-tina.color('black')
-tina.pendown()
-tina.circle(10)
-tina.penup()
-tina.end_fill()
-
-#Draw mounth
-tina.begin_fill()
-tina.color('red')
-tina.goto(-20, -50)
-tina.pendown()
-tina.right(70)
-tina.circle(40, 220)
-tina.goto(-20, -50)
-tina.penup()
-tina.end_fill()
+tina.color("green") 
+for number in number_list: 
+    tina.forward(number*10) 
+    tina.left(60)
 
 turtle.done()
