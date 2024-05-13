94571e6d166beb30bf25a3ce5eff7c33d32f1577
Leda909 <ko9zad@gmail.com>
2024-05-13 15:54:57 +0100

    for loop on color and number

--- index.py
+++ index.py
@@ -1,12 +1,16 @@
 import turtle
+
 tina = turtle.Turtle()
 tina.shape('turtle')
 
-number_list = [1,2,3,4,5,6,7,8,9,10] 
+number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+color_list = ["blue", "purple", "green"]
 
-tina.color("green") 
-for number in number_list: 
-    tina.forward(number*10) 
-    tina.left(60)
+for color in color_list:
+    tina.color(color)
+    tina.write(color)
+    for number in number_list:
+        tina.forward(number * 10)
+        tina.left(60)
 
 turtle.done()
