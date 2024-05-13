88bcf9ec7fa9cd7f9f31cee468bd5185f239e81f
Leda909 <ko9zad@gmail.com>
2024-05-13 17:35:22 +0100

    3 candles

--- index.py
+++ index.py
@@ -1,26 +1,30 @@
 import turtle
-
-tina = turtle.Turtle()
+tina=turtle.Turtle()
 tina.shape('turtle')
-tina.speed(3)  # Set the speed
 
-color_list = ["red", "blue", "green", "orange", "purple", "yellow"]
-
-# Draw a triangle with a given color
-def draw_triangle(color):
-    tina.color(color)  # Set the color of the triangle
-    tina.begin_fill()  # Start filling the triangle
-    for _ in range(3):
-        tina.forward(30)
-        tina.left(120)
-    tina.end_fill()  # End filling the triangle
-
-# Draw a hexagon composed of triangles, each with a different color
-for i, color in enumerate(color_list):
+def make_cake(x=0, y=0):
     tina.penup()
-    tina.goto(0, 0)  # Go back to the center of the circle
-    tina.setheading(60 * i)  # Set the heading for the next triangle
+    tina.color('pink')
+    tina.goto(x, y)
     tina.pendown()
-    draw_triangle(color)
+    tina.begin_fill()
+    tina.goto(x + 20, y)
+    tina.goto(x + 20, y + 20)
+    tina.goto(x - 20, y + 20)
+    tina.goto(x - 20, y)
+    tina.goto(x, y)  
+    tina.end_fill()
+    tina.goto(x, y + 20)
+    tina.color('yellow')
+    tina.goto(x, y + 35)
+    tina.goto(x, y + 30)
+    tina.color('black')
+    tina.goto(x, y + 20)
+    tina.penup()
+    tina.goto(x, y + 10)
 
-turtle.done()
+make_cake(0,0)
+make_cake(-100,0)
+make_cake(100,0)
+
+tina.done()
