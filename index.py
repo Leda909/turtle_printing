3138ce477665b5f9634763fd5ec22178fd8ec2b2
Leda909 <ko9zad@gmail.com>
2024-05-13 17:33:36 +0100

    hehagon from triangles

--- index.py
+++ index.py
@@ -1,16 +1,26 @@
 import turtle
-tina=turtle.Turtle()
+
+tina = turtle.Turtle()
 tina.shape('turtle')
-tina.color('purple')
+tina.speed(3)  # Set the speed
 
-def triangle():
-    tina.left(60)
-    tina.forward(30)
-    tina.left(120)
-    tina.forward(30)
-    tina.left(120)
-    tina.forward(30)
+color_list = ["red", "blue", "green", "orange", "purple", "yellow"]
 
-triangle()
+# Draw a triangle with a given color
+def draw_triangle(color):
+    tina.color(color)  # Set the color of the triangle
+    tina.begin_fill()  # Start filling the triangle
+    for _ in range(3):
+        tina.forward(30)
+        tina.left(120)
+    tina.end_fill()  # End filling the triangle
+
+# Draw a hexagon composed of triangles, each with a different color
+for i, color in enumerate(color_list):
+    tina.penup()
+    tina.goto(0, 0)  # Go back to the center of the circle
+    tina.setheading(60 * i)  # Set the heading for the next triangle
+    tina.pendown()
+    draw_triangle(color)
 
 turtle.done()
