0e971dc26ea3315d9181edc780fd11a7bf4c6568
Leda909 <ko9zad@gmail.com>
2024-05-13 16:50:45 +0100

    input

--- index.py
+++ index.py
@@ -1,23 +1,16 @@
 import turtle
 
 tina = turtle.Turtle()
-tina.shape('turtle')
-tina.color('orange')
 
-tina.left(90)
-tina.forward(100)
-tina.write("I'm Tina!")
-tina.forward(20)
-tina.right(90)
+Guess = int(input("What is 2 X 7?"))
 
-tommy = turtle.Turtle()
-tommy.shape('turtle')
-tommy.color('blue')
-
-tommy.right(90)
-tommy.forward(100)
-tommy.write("I'm Tommy!")
-tommy.forward(20)
-tommy.left(90)
+if Guess == 2*7:
+    tina.write(str(Guess) + ' is correct!')
+    tina.penup()
+    tina.backward(10)
+else:
+    tina.write('You said ' + str(Guess) + '. I got ' + str(2*7))
+    tina.penup()
+    tina.backward(10)
 
 turtle.done()
