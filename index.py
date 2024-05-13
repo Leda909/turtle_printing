4001102d8590b7a66ad7b7d35c96b3b58bff4566
Leda909 <ko9zad@gmail.com>
2024-05-13 16:44:18 +0100

    changing color by for_each

--- index.py
+++ index.py
@@ -1,16 +1,14 @@
 import turtle
-
 tina = turtle.Turtle()
 tina.shape('turtle')
 
-number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-color_list = ["blue", "purple", "green"]
+colors = ["red", "orange", "yellow", "green", "blue", "purple", "black"]
 
-for color in color_list:
-    tina.color(color)
-    tina.write(color)
-    for number in number_list:
-        tina.forward(number * 10)
-        tina.left(60)
+for each_color in colors:
+    angle = 360 / len(colors)
+    tina.color(each_color)
+    tina.circle(40)
+    tina.right(angle)
+    tina.forward(30)
 
 turtle.done()
